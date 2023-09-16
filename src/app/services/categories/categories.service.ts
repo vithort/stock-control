cb8a5341810a793d8976e1e466ec2e8aa9040dfe
Vithor Carvalho <vithort@gmail.com>
2023-09-16 20:33:51 +0100

    43. implementa service de categories e método de buscar categorias

---
+++ src/app/services/categories/categories.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CookieService } from 'ngx-cookie-service';
+
+import { environment } from 'src/environments/environment';
+
+import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/GetCategoriesResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  private API_URL = environment.API_URL;
+  private JWT_TOKEN = this.cookieService.get('USER_INFO');
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.JWT_TOKEN}`,
+    }),
+  };
+
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
+
+  getAllCategories(): Observable<GetCategoriesResponse[]> {
+    return this.http.get<GetCategoriesResponse[]>(
+      `${this.API_URL}/categories`,
+      this.httpOptions
+    );
+  }
+}
