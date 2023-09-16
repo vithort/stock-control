7cdd8cea1ce968da672965c0f1539cd5500728e3
Vithor Carvalho <vithort@gmail.com>
2023-09-16 13:54:42 +0100

    21. implementando layaout da tela de login - parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { CardModule } from 'primeng/card';
+
+import { InputTextModule } from 'primeng/inputtext';
+
+import { ButtonModule } from 'primeng/button';
+
+import { ToastModule } from 'primeng/toast';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    // PrimeNG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
