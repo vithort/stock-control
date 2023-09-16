e17992005ce3a762f043df8179c979923a0933ce
Vithor Carvalho <vithort@gmail.com>
2023-09-16 17:17:10 +0100

    33. implementando funcionalidade de logout

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,8 +1,17 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
   styleUrls: [],
 })
-export class ToolbarNavigationComponent {}
+export class ToolbarNavigationComponent {
+  constructor(private cookieService: CookieService, private router: Router) {}
+
+  handleLogout(): void {
+    this.cookieService.delete('USER_INFO');
+    void this.router.navigate(['/home']);
+  }
+}
