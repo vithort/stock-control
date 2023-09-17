0992c1e16db80427fe8c8376ac7a3d6bc0178ba1
Vithor Carvalho <vithort@gmail.com>
2023-09-17 13:44:01 +0100

    57. implementa funcionalidade de editar categoria

--- src/app/modules/categories/components/category-form/category-form.component.ts
+++ src/app/modules/categories/components/category-form/category-form.component.ts
@@ -31,7 +31,67 @@ export class CategoryFormComponent implements OnInit, OnDestroy {
     public ref: DynamicDialogConfig
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.categoryAction = this.ref.data;
+
+    if (
+      (this.categoryAction?.event?.action === this.editCategoryAction &&
+        this.categoryAction?.event?.id !== null) ||
+      undefined
+    ) {
+      this.setCategoryName(this.categoryAction?.event?.categoryName as string);
+    }
+  }
+
+  handleSubmitCategoryAction(): void {
+    if (this.categoryAction?.event?.action === this.addCategoryAction) {
+      this.handleSubmitAddCategory();
+    } else if (this.categoryAction?.event?.action === this.editCategoryAction) {
+      this.handleSubmitEditCategory();
+    }
+
+    return;
+  }
+
+  handleSubmitEditCategory(): void {
+    if (
+      this.categoryForm?.value &&
+      this.categoryForm?.valid &&
+      this.categoryAction?.event?.id
+    ) {
+      const requestEditCategory: { name: string; category_id: string } = {
+        name: this.categoryForm?.value?.name as string,
+        category_id: this.categoryAction?.event?.id as string,
+      };
+
+      this.categoriesService
+        .editCategoryName(requestEditCategory)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            this.categoryForm.reset();
+
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Categoria editada com sucesso!',
+              life: 2500,
+            });
+          },
+          error: (err) => {
+            console.error(err);
+            this.categoryForm.reset();
+
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao editar categoria!',
+              life: 2500,
+            });
+          },
+        });
+    }
+  }
 
   handleSubmitAddCategory(): void {
     if (this.categoryForm?.value && this.categoryForm?.valid) {
@@ -67,6 +127,14 @@ export class CategoryFormComponent implements OnInit, OnDestroy {
             });
           },
         });
+    }
+  }
+
+  setCategoryName(categoryName: string): void {
+    if (categoryName) {
+      this.categoryForm.setValue({
+        name: categoryName,
+      });
     }
   }
 
--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -48,4 +48,20 @@ export class CategoriesService {
       this.httpOptions
     );
   }
+
+  editCategoryName(requestData: {
+    category_id: string;
+    name: string;
+  }): Observable<void> {
+    return this.http.put<void>(
+      `${this.API_URL}/category/edit`,
+      { name: requestData?.name },
+      {
+        ...this.httpOptions,
+        params: {
+          category_id: requestData?.category_id,
+        },
+      }
+    );
+  }
 }
